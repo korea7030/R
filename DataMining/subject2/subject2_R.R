d17363ac6f00a4099d616d47555dcd032e333014
korea7030 <korea7030@naver.com>
2016-06-12 20:40:03 +0900

    20160612 commit

--- DataMining/subject2/subject2_R.R
+++ DataMining/subject2/subject2_R.R
@@ -1,44 +1,58 @@
 catalog <- read.csv('catalogCrossSell.csv', stringsAsFactors = FALSE)
 head(catalog)
-catalog <- catalog[,-c(1,4)]
+summary(catalog)
+catalog <- catalog[,-c(1,4)] # health의 경우 산 경우만 있기 때문에 제외
+head(catalog)
 
-## cramer'sV 명목형 변수 상관계수 함수
-cv.test = function(x,y) {
-  CV = sqrt(chisq.test(x, y, correct=FALSE)$statistic /
-              (length(x) * (min(length(unique(x)),length(unique(y))) - 1)))
-  print.noquote("Cramér V / Phi:")
-  return(as.numeric(CV))
-}
+### jaccard coefficient 관련 package ################################
+# install.packages("prabclus")
+library(prabclus)
+jaccard(as.matrix(catalog))
 
-### 의류 상관계수 
-with(catalog, cv.test(Clothing, Housewares));with(catalog, cv.test(Clothing, Automotive));with(catalog, cv.test(Clothing, Electronics)); # 의류와 자동차, 전자제품
-with(catalog, cv.test(Clothing, Computers));with(catalog, cv.test(Clothing, Garden));with(catalog, cv.test(Clothing, Gift)); # 의류와 원예용품, 의류와 선물
-with(catalog, cv.test(Clothing, Jewelry)); # 의류와 쥬얼리
-### 가정용품 상관계수 
-with(catalog, cv.test(Housewares, Clothing));with(catalog, cv.test(Housewares, Automotive));with(catalog, cv.test(Housewares, Electronics)); # 가정용품과 자동차
-with(catalog, cv.test(Housewares, Computers));with(catalog, cv.test(Housewares, Garden));with(catalog, cv.test(Housewares, Gift));
-with(catalog, cv.test(Housewares, Jewelry));
-### 자동차 상관계수 
-with(catalog, cv.test(Automotive, Clothing));with(catalog, cv.test(Automotive, Housewares));with(catalog, cv.test(Automotive, Electronics)); # 자동차와 의류, 가정용품
-with(catalog, cv.test(Automotive, Computers));with(catalog, cv.test(Automotive, Garden));with(catalog, cv.test(Automotive, Gift)); # 자동차와 컴퓨터
-with(catalog, cv.test(Automotive, Jewelry));
-### 전자제품 상관계수 
-with(catalog, cv.test(Electronics, Clothing));with(catalog, cv.test(Electronics, Housewares));with(catalog, cv.test(Electronics, Automotive)); # 전자제품과 의류
-with(catalog, cv.test(Electronics, Computers));with(catalog, cv.test(Electronics, Garden));with(catalog, cv.test(Electronics, Gift)); # 전자제품과 컴퓨터
-with(catalog, cv.test(Electronics, Jewelry));
-### 컴퓨터 상관계수 
-with(catalog, cv.test(Computers, Clothing));with(catalog, cv.test(Computers, Housewares));with(catalog, cv.test(Computers, Automotive)); # 컴퓨터와 자동차
-with(catalog, cv.test(Computers, Electronics));with(catalog, cv.test(Computers, Garden));with(catalog, cv.test(Computers, Gift)); # 컴퓨터와 전자제품, 원예용품, 선물
-with(catalog, cv.test(Computers, Jewelry));
-### 원예용품 상관계수 
-with(catalog, cv.test(Garden, Clothing));with(catalog, cv.test(Garden, Housewares));with(catalog, cv.test(Garden, Automotive)); # 원예와 의류
-with(catalog, cv.test(Garden, Electronics));with(catalog, cv.test(Garden, Computers));with(catalog, cv.test(Garden, Gift)); # 원예와 컴퓨터
-with(catalog, cv.test(Garden, Jewelry));
-### 선물 상관계수 
-with(catalog, cv.test(Gift, Clothing));with(catalog, cv.test(Gift, Housewares));with(catalog, cv.test(Gift, Automotive)); # 선물과 의류
-with(catalog, cv.test(Gift, Electronics));with(catalog, cv.test(Gift, Computers));with(catalog, cv.test(Gift, Garden)); # 선물과 컴퓨터
-with(catalog, cv.test(Gift, Jewelry));
-### 쥬얼리 상관계수 
-with(catalog, cv.test(Jewelry, Clothing));with(catalog, cv.test(Jewelry, Housewares));with(catalog, cv.test(Jewelry, Automotive)); # 쥬얼리와 의류
-with(catalog, cv.test(Jewelry, Electronics));with(catalog, cv.test(Jewelry, Computers));with(catalog, cv.test(Jewelry, Garden)); 
-with(catalog, cv.test(Jewelry, Gift));
+## 의류, 가정용품, 자동차, 전자제품, 컴퓨터, 원예, 선물, 쥬얼리(1,2,3,4,5,6,7,8)
+### 각각 용품별 상품을 구분 했으나 의류와 상관계수가 높은 용품이 거의 없다고 판단하여 전체를 넣고 kmodes 수행 ###
+catalog1 <- catalog
+
+## 최적의 군집수 찾기(명목형 변수)
+# library(klaR)
+set.seed(1)
+wss <- 0
+for (i in 1:15) wss[i] <- kmodes(catalog1, i)$withindiff # 군집내 분산값
+### 기울기가 급격하게 감소하는 지점에서 군집의 수 지정
+plot(1:15, wss, type="b", xlab = "# of clusters ", ylab = "Within group sum of squares") # 최적의 군집 : 12
+
+#### 
+
+### 최적의 군집수 찾은 후 군집 진행
+km <- kmodes(catalog1, 12)
+clusplot(catalog1, km$cluster)
+
+### 
+
+### SOM 군집 활용
+### 12개의 SOM으로 최대한 나타낼 수 있는 r과 c는 각각 3 으로 지정
+library(kohonen)
+str(catalog1)
+set.seed(1) 
+catalog1.n <- scale(catalog1)
+sm <- som(data = catalog1.n , grid = somgrid(3,3,"rectangular"), rlen=13) # row, column , 사각형
+str(sm)
+plot(sm, main="Catalog")
+
+### 군집별 세분화변수 표시
+catalog1$cluster <- sm$unit.classif
+plot(xtabs(~cluster+Clothing, data=catalog1), main="Clothing plot")
+plot(xtabs(~cluster+Housewares, data=catalog1), main="Housewares plot")
+plot(xtabs(~cluster+Automotive, data=catalog1), main="Automotive plot")
+plot(xtabs(~cluster+Electronics, data=catalog1), main="Electronics plot")
+plot(xtabs(~cluster+Computers, data=catalog1), main="Computers plot")
+plot(xtabs(~cluster+Garden, data=catalog1), main="Garden plot")
+plot(xtabs(~cluster+Gift, data=catalog1), main="Gift plot")
+plot(xtabs(~cluster+Jewelry, data=catalog1), main="Jewelry plot")
+
+### x,y 좌표 표시
+library(ggplot2)
+catalog1$clusterX <- sm$grid$pts[sm$unit.classif,"x"] # x 값
+catalog1$clusterY <- sm$grid$pts[sm$unit.classif, "y"] # y 값
+p <- ggplot(catalog1, aes(clusterX, clusterY)) 
+p+geom_jitter(position = position_jitter(width=.2, height=.2))
